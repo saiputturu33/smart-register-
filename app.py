1491af5c189287493a969da4f77fb3d7dd11733b
saiputturu33 <saiputturu44@gmail.com>
2025-07-12 09:52:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+ """
+ Renders the main index.html template for the Smart Register applicaƟon.
+ """
+ return render_template('index.html')
+if __name__ == '__main__':
+ # Run the Flask applicaƟon in debug mode for development
+ app.run(debug=True)
